1f0d62ad0613bb327179babeadc807b78db89425
LDorine <dodosav.fr@gmail.com>
2019-03-25 11:21:29 +0100

    rename pages and reorganization

---
+++ DigitalEnvironment.php
@@ -0,0 +1,151 @@
+<?php require 'header.php'; ?>
+<div class="container">
+<!-- menu burger -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="homecarousel.php">Home<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="euratech.php">Euratech</a>
+                <a class="nav-item nav-link active" href="DigitalEnvironment.php">Digital Environment</a>
+                <a class="nav-item nav-link" href="formContact.php">Contact us</a>
+            </div>
+        </div>
+    </nav>         
+    <div class="row-1">
+        <div class="span-3 titre1">
+            <h1><span class ="titrepage">Digital Environment </span></h1>
+        </div>
+    </div>
+    <div class="row-2">
+        <div class="span-2 offset-2 header">
+            <p>Home / Euratechnologie / DigitalEnvironment</p>
+        </div>
+    </div>
+    <section class="history_euratech">
+        <div class="row-3">
+            <div class="col-9 offset-2">
+                <h2><span class="subtitle">other hives of companies on the lille metropolis</span></h2>
+            </div>
+        </div>
+        <div class="row justify-content">
+            <div clas="col-9 offset-2">
+                <div class="col-8 offset-2">
+                    <p>
+                        There are different digital environments in addition to incubators such as corporate hives, corporate hothouses and activity areas.
+                        Incubators are support structures for very young company creation projects.
+                        Corporate Hives is a business that provides beginner companies with physical and relational tools for growth.
+                        Corporate hothouses start-up companies with financial and structural support.
+                        The areas correspond to the viable business location.
+                    </p>
+                </div>  
+            </div>    
+        </div>
+    </section>
+    <section class="our_select">
+        <div class="row-5">
+            <div class="span-3 offset-5">
+                <h2><span class="subtitle">where ? when ? who ?</span></h2>
+            </div>
+        </div>               
+        <div class="row">
+            <div class="col-2 offset-2">
+                <img src="picture/logoEnv/logo-blanchemaille.png" alt="logo blanchemaille">
+            </div>
+            <div class="col-4">
+                <h3>Blanchemaille <small>by Euratechnologies</small></h3>
+                <p>
+                    Blanchemaille is a subsidiary of euratechnologie dedicated to e-commerce in Roubaix. 
+                    On is located near the major players of e-commerce Private Showroom, OVH and many other .
+                    The e-commerce companies extend over 13000m² with a school of trade.
+                </p>
+                <p>Adrress : 87 rue du Fontenoy 59100 Roubaix</p>
+                <a href="https://blanchemaille-euratechnologies.com/"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2 offset-6">
+                <img src="picture/logoEnv/logo-plaineimage.png" alt="logo plaine images">
+            </div>
+            <div class="col-4">
+                <h3>Plaine images </h3>
+                <p>
+                    Plain Image is a complete ecosystem on the creative industry in Tourcoing. 
+                    Participating companies in this space are in the game, audio-visual, virtual reality or design. The big companies are Ankama, Wakanim, Cream french ... 
+                    There are 125 companies on 40 000m² with 3 schools.
+                </p>
+                <p>Adrress : 99 A boulevard Constantin Descat 59200 Tourcoing</p>
+                <a href="https://www.plaine-images.fr/"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2 offset-2">
+                <img src="picture/logoEnv/logo-rucheentreprise.png" alt="logo ruche entreprise">
+            </div>
+            <div class="col-4">
+                <h3>Ruche d'entreprise</h3>
+                <p>
+                    Corporate hives welcome companies in creation and evolution.
+                    There are companies with diverse and various projects. 
+                    In Lille metropolis there are 4 hives located in Hellemmes, Villeneuve d'Ascq, Tourcoing and Armentieres.
+                </p>
+                <a href="https://www.lillemetropole.fr/fr/ruches-dentreprises"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2 offset-6">
+                <img src="picture/logoEnv/logo-creinnov.png" alt="logo creinnov">
+            </div>
+            <div class="col-4">
+                <h3>Cre'innov</h3>
+                <p>
+                    Cre'innov is an incubator in relation with the University of Lille.
+                    The business district of Villeneuve d'Asq is located close to all its businesses as well as the corporate hive.
+                </p>
+                <p>Adrress : 11 rue de l’Harmonie 59650 Villeneuve d’ascq</p>
+                <a href="http://cre-innov.univ-lille1.fr/Presentation/"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2 offset-2">
+                <img src="picture/logoEnv/logo-serrenumerique.png" alt="logo serre numerique">
+            </div>
+            <div class="col-8">
+                <h3>Serre numerique</h3>
+                <p>
+                    The digital greenhouse in Valenciennes is an ecosystem on the professions of image and digital creation. 
+                    There are 17,000m² with 3 schools. 
+                </p>
+                <p>Adrress : 2 rue Peclet 59300 Valenciennes</p>
+                <a href="http://www.serre-numerique.fr/"></a>
+            </div>    
+        </div>       
+    </section>  
+    <!-- map-environnement -->
+    <section>
+        <div class="row">
+            <div class="col-10 offset-3">
+                <div class="map_faci">
+                    <h2><span class="subtitle">Facilities and transports services</span></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57257.58747735495!2d3.047054053682592!3d50.63732754614762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d55c764ab439%3A0x2e9bfd5c48d283b!2sEuraTechnologies!5e0!3m2!1sfr!2sfr!4v1551964680317" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col offset-1">
+                <h3>How to join us</h3>
+                <ul>
+                    <li>Subway Line 2 - Canteleu stop - 10mn walk away</li>
+                    <li>Bus Line 18 - Euratechnologie stop </li>
+                </ul>
+            </div>   
+        </div>  
+    </section> 
+</div>
+<?php require 'footer.php'; ?>
--- formContact.php
+++ formContact.php
@@ -63,187 +63,137 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
   }
 }
+
+require 'header.php'; 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>WILD - Wild Information Of the Lille's Digital  </title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link href="cssGlobal.css" rel="stylesheet" type="text/css" />
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-  <meta charset="utf-8" />
-</head>
-<body>
-  <div class="container ">
+<div class="container ">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link" href="homecarousel.html">Home<span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="euratech.html">Euratech</a>
-          <a class="nav-item nav-link" href="DigitalEnvironment.html">Digital Environment</a>
-          <a class="nav-item nav-link active" href="formContact.html">Contact us</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="homecarousel.php">Home<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="euratech.php">Euratech</a>
+                <a class="nav-item nav-link" href="DigitalEnvironment.php">Digital Environment</a>
+                <a class="nav-item nav-link active" href="formContact.php">Contact us</a>
+            </div>
         </div>
-      </div>
     </nav>
     <div class="row-1">
-      <div class="span-3 titre1">
-        <h1><span class="titrepage">Contact Us</span></h1>
-      </div>   
+        <div class="span-3 titre1">
+            <h1><span class="titrepage">Contact Us</span></h1>
+        </div>   
     </div>   
     <div class="row-2">
-      <div class="span-2 offset-2 header">
-        <p>Home / Euratechnologie / DigitalEnvironment / </p>
-      </div>
+        <div class="span-2 offset-2 header">
+            <p>Home / Euratechnologie / DigitalEnvironment / </p>
+        </div>
     </div> 
     <?php if(isset($_GET['new_success'])): ?>
-        <div class='alert alert-success'> 
-            Your newsletter inscription is done !
-        </div>
+    <div class='alert alert-success'> 
+    Your newsletter inscription is done !
+    </div>
     <?php endif; ?>
     <?php if(isset($_GET['mail_success'])): ?>
-        <div class='alert alert-success'> 
-            Your contact message is done !
-        </div>
+    <div class='alert alert-success'> 
+    Your contact message is done !
+    </div>
     <?php endif; ?>
     <div class="changeIMG">
-      <div class="row">
-        <div class="span offset-4">
-            <h6 class="newletter_title">Sign up to our Newsletter !</h6>
-        </div>
-      </div>
-      <form action="formContact.php" method="post" id="newletter" class="newletter">
         <div class="row">
-          <div class="col-9 offset-1">
-            <input type="text" id="nameNew" name="nameNew" class="name" placeholder="Name :" size="70" maxlength="30"/>
-            <?php  if(isset($errorsNews['emptyName'])) : ?>
-            <p class="form-text error"><?= $errorsNews['emptyName']; ?></p>
-            <?php endif; ?>
-            <?php  if(isset($errorsNews['incorrectName'])) : ?>
-            <p class="form-text error"><?= $errorsNews['incorrectName']; ?></p>
-            <?php endif; ?>
-          </div>
+            <div class="span offset-4">
+                <h6 class="newletter_title">Sign up to our Newsletter !</h6>
+            </div>
         </div>
-        <div class="row">
-          <div class="col-9 offset-1">
-            <input type="text" id="mailNew" name="mailNew" class="mail_new" placeholder="Mail :" size="70" maxlength="30" />
-            <?php  if(isset($errorsNews['emptyMail'])) : ?>
-            <p class="form-text error"><?= $errorsNews['emptyMail']; ?></p>
-            <?php endif; ?>
-            <?php  if(isset($errorsNews['incorrectMail'])) : ?>
-            <p class="form-text error"><?= $errorsNews['incorrectMail']; ?></p>
-            <?php endif; ?>
-          </div>
-        </div>    
-        <div class="row">
-          <div class="col-3 offset-9">
-            <input type="submit" class="send_newsletter" value="SEND"/>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div class="row">
-      <div class="col-6 offset-5">
-        <h2><span class="subtitle">Contact Us</span></h2>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-9 offset-1"> 
-        <p class="paragraph">
-          <span class="bold">HEY !</span><br>
-          Do you want us to meet?
-          Would you like to share your incredible talents and join one of our companies?
-          Would you like to learn the trade and do an internship with us?
-          Or simply ask us a question?
-          <span class="bold">Contact us with this form:</span>
-        </p>
-      </div>
-      <div class="contact">
-        <form action="formContact.php" method="post" id="contactPost" class="contactPost">
-          <div class="row">
-            <div class="col-9 offset-1">
-              <select  id="choixContact" name="choixContact">
-                <option value="I am ...">I am ...</option>
-                <option value="I am an incubator">I am an incubator</option>
-                <option value="I am a company">I am a company</option>
-                <option value="I am looking for an internship">I am looking for an internship</option>
-                <option value="I am looking for a formation">I am looking for a formation</option>
-              </select> 
-              <?php  if(isset($errorsContact['emptychoice'])) : ?>
-              <p class="form-text error"><?= $errorsContact['emptychoice']; ?></p>
-              <?php endif; ?>
+        <form action="formContact.php" method="post" id="newletter" class="newletter">
+            <div class="row">
+                <div class="col-9 offset-1">
+                    <input type="text" id="nameNew" name="nameNew" class="name" placeholder="Name :" size="70" maxlength="30"/>
+                    <?php  if(isset($errorsNews['emptyName'])) : ?>
+                    <p class="form-text error"><?= $errorsNews['emptyName']; ?></p>
+                    <?php endif; ?>
+                    <?php  if(isset($errorsNews['incorrectName'])) : ?>
+                    <p class="form-text error"><?= $errorsNews['incorrectName']; ?></p>
+                    <?php endif; ?>
+                </div>
             </div>
-          </div>
-          <div class="row">
-              <div class="col-9 offset-1">
-                <input type="text" id="mailContact" name="mailContact" class="mail_contact" placeholder="@ :" size="70" maxlength="30" />
-                <?php  if(isset($errorsContact['emptyMail'])) : ?>
-                <p class="form-text error"><?= $errorsContact['emptyMail']; ?></p>
-                <?php endif; ?>
-                <?php  if(isset($errorsContact['incorrectMail'])) : ?>
-                <p class="form-text error"><?= $errorsContact['incorrectMail']; ?></p>
-                <?php endif; ?>
-              </div>
-          </div>
-          <div class="row">
-            <div class="col-9 offset-1">
-              <textarea id="messageContact" name="messageContact" class="message" cols="70" rows="10"></textarea>
-              <?php  if(isset($errorsContact['emptyMessage'])) : ?>
-              <p class="form-text error"><?= $errorsContact['emptyMessage']; ?></p>
-              <?php endif; ?>
+            <div class="row">
+                <div class="col-9 offset-1">
+                    <input type="text" id="mailNew" name="mailNew" class="mail_new" placeholder="Mail :" size="70" maxlength="30" />
+                    <?php  if(isset($errorsNews['emptyMail'])) : ?>
+                    <p class="form-text error"><?= $errorsNews['emptyMail']; ?></p>
+                    <?php endif; ?>
+                    <?php  if(isset($errorsNews['incorrectMail'])) : ?>
+                    <p class="form-text error"><?= $errorsNews['incorrectMail']; ?></p>
+                    <?php endif; ?>
+                </div>
+            </div>    
+            <div class="row">
+                <div class="col-3 offset-9">
+                    <input type="submit" class="send_newsletter" value="SEND"/>
+                </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-3 offset-9">
-                <input type="submit" value="SEND" class="send_contact" />
-            </div>
-          </div>
         </form>
-      </div>
-      <footer class="page-footer font-small">
-          <!-- Footer Links -->
-          <div class="container">
-            <!-- Grid row-->
-            <div class="row text-center d-flex justify-content-center pt-5 mb-3 footer">
-              <div class="col-md-2 mb-3">
-                <h6 class="text-uppercase font-weight-bold">
-                  <a href="homecarousel.html">About us</a>
-                </h6>
-              </div>
-              <div class="col-md-2 mb-3">
-                <h6 class="text-uppercase font-weight-bold">
-                  <a href="#!">Legal notice</a>
-                </h6>
-              </div>
-              <div class="col-md-2 mb-3">
-                <h6 class="text-uppercase font-weight-bold">
-                  <a href="#!">Annex</a>
-                </h6>
-              </div>
-              <div class="col-md-2 mb-3">
-                <h6 class="text-uppercase font-weight-bold">
-                  <a href="#!">Help</a>
-                </h6>
-              </div>
-              <div class="col-md-2 mb-3">
-                <h6 class="text-uppercase font-weight-bold">
-                  <a href="formContact.html">Contact</a>
-                </h6>
-              </div> 
+    </div>
+    <div class="row">
+        <div class="col-6 offset-5">
+            <h2><span class="subtitle">Contact Us</span></h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-9 offset-1"> 
+            <p class="paragraph">
+            <span class="bold">HEY !</span><br>
+            Do you want us to meet?
+            Would you like to share your incredible talents and join one of our companies?
+            Would you like to learn the trade and do an internship with us?
+            Or simply ask us a question?
+            <span class="bold">Contact us with this form:</span>
+            </p>
+        </div>
+    </div>
+    <div class="contact">
+        <form action="formContact.php" method="post" id="contactPost" class="contactPost">
+            <div class="row">
+                <div class="col-9 offset-1">
+                    <select  id="choixContact" name="choixContact">
+                        <option value="I am ...">I am ...</option>
+                        <option value="I am an incubator">I am an incubator</option>
+                        <option value="I am a company">I am a company</option>
+                        <option value="I am looking for an internship">I am looking for an internship</option>
+                        <option value="I am looking for a formation">I am looking for a formation</option>
+                    </select> 
+                    <?php  if(isset($errorsContact['emptychoice'])) : ?>
+                    <p class="form-text error"><?= $errorsContact['emptychoice']; ?></p>
+                    <?php endif; ?>
+                </div>
             </div>
-            <hr class="rgba-white-light" style="margin: 0 15%;">        
-            <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">   
-            <div class="row pb-3">   
+            <div class="row">
+                <div class="col-9 offset-1">
+                    <input type="text" id="mailContact" name="mailContact" class="mail_contact" placeholder="@ :" size="70" maxlength="30" />
+                    <?php  if(isset($errorsContact['emptyMail'])) : ?>
+                    <p class="form-text error"><?= $errorsContact['emptyMail']; ?></p>
+                    <?php endif; ?>
+                    <?php  if(isset($errorsContact['incorrectMail'])) : ?>
+                    <p class="form-text error"><?= $errorsContact['incorrectMail']; ?></p>
+                    <?php endif; ?>
+                </div>
             </div>
-          </div>
-          <div class="footer-copyright text-center py-3">© 2018 Copyright: The club 5!
-          </div>
-          <!-- Copyright -->
-      
-        </footer>
-    </div> 
-  </div>
-</body>
-</html>
+            <div class="row">
+                <div class="col-9 offset-1">
+                    <textarea id="messageContact" name="messageContact" class="message" cols="70" rows="10"></textarea>
+                    <?php  if(isset($errorsContact['emptyMessage'])) : ?>
+                    <p class="form-text error"><?= $errorsContact['emptyMessage']; ?></p>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3 offset-9">
+                    <input type="submit" value="SEND" class="send_contact" />
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<?php require 'footer.php'; ?>
