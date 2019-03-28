eb915ccd542704af484a53ded688ca3e55fc43ff
EmilieVilliere <you@example.com>
2019-03-28 18:14:51 +0100

    Adding all finition

---
+++ annex.php
@@ -0,0 +1,45 @@
+<?php require 'header.php'; ?>
+    <!-- menu burger -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-item nav-link" href="homecarousel.php">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link active" href="euratech.php">Euratech</a>
+            <a class="nav-item nav-link" href="DigitalEnvironment.php">Digital Environment</a>
+            <a class="nav-item nav-link" href="formContact.php">Contact us</a>
+            </div>
+        </div>
+    </nav>
+
+<div>
+<h1>LIENS DIGITAL ENV :</h1> 
+
+<a href="https://www.lafrenchtech.com/en/">https://www.lafrenchtech.com/en/</a><br>
+<a href="https://en.wikipedia.org/wiki/Internet_exchange_point">https://en.wikipedia.org/wiki/Internet_exchange_point</a><br>
+<a href=">http://www.fundacity.com/european-accelerator-report-2014">>http://www.fundacity.com/european-accelerator-report-2014</a><br>
+
+<h1>LIENS ENTREPRISES EURATECH : </h1>
+<a href="https://www.euratechnologies.com/incubateur-d-entreprises/">https://www.euratechnologies.com/incubateur-d-entreprises/</a><br>
+<a href="https://www.euratechnologies.com/accelerateur-de-startups/">https://www.euratechnologies.com/accelerateur-de-startups/</a><br>
+<a href="https://www.euratechnologies.com/hotel-dentreprises/">https://www.euratechnologies.com/hotel-dentreprises/</a><br>
+<a href="https://www.euratechnologies.com/entreprises/microsoft/">https://www.euratechnologies.com/entreprises/microsoft/</a><br>
+<a href="https://www.euratechnologies.com/entreprises/capgemini/">https://www.euratechnologies.com/entreprises/capgemini/</a><br>
+<a href="https://www.euratechnologies.com/entreprises/ibm-services-center/">https://www.euratechnologies.com/entreprises/ibm-services-center/</a><br>
+<a href="https://www.euratechnologies.com/entreprises/soft-computing/">https://www.euratechnologies.com/entreprises/soft-computing/</a><br>
+<a href="https://www.euratechnologies.com/entreprises/clic2buy/">https://www.euratechnologies.com/entreprises/clic2buy/</a><br>
+<a href="https://www.euratechnologies.com/entreprises/arobas-music/">https://www.euratechnologies.com/entreprises/arobas-music/</a><br>
+
+
+<h1>LIENS RESTO/BAR : </h1>
+
+<a href="http://alexens.com/"></a>
+<a href="https://www.bagelcorner.fr/">https://www.bagelcorner.fr/</a>
+<a href="https://www.sogood.fr/content/18-livraison-plateau-repas-euratechnologies">https://www.sogood.fr/content/18-livraison-plateau-repas-euratechnologies</a><br>
+<a href="https://www.facebook.com/pages/category/European-Restaurant/Nostrum-Euratechnologies-256870324886697/">https://www.facebook.com/pages/category/European-Restaurant/Nostrum-Euratechnologies-256870324886697/</a><br>
+<a href="http://www.enviesdesaison.fr/">http://www.enviesdesaison.fr/</a>
+
+
+</div>
