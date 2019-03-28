469a724d947885db36154cb5b2245672986eb83c
Louis Allard <l.allard@yahoo.fr>
2019-03-28 17:09:49 +0100

    add aboutus

---
+++ aboutus.php
@@ -0,0 +1,129 @@
+<style>
+.aboutus {
+    width: 100%;
+}
+
+.card {
+    width: 250px;
+    height: 400px;
+    border : 1px solid #E7E7E7;
+    float: left;
+    margin: 5px;
+}
+
+.pic {
+    width: 250px;
+    height: 100px;
+    border-bottom: 3px solid #E7E7E7;
+    padding-top : 5px;
+}
+
+img {
+    width : 90px;
+    margin-left: 80px;
+}
+
+li {
+    list-style-type: square;
+}
+</style>
+<link rel="css" src="cssGlobal.css" />
+<body>
+
+<div class="container">
+                <!-- menu burger -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="homecarousel.php">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link active" href="euratech.php">Euratech</a>
+            <a class="nav-item nav-link" href="DigitalEnvironment.php">Digital Environment</a>
+            <a class="nav-item nav-link" href="formContact.php">Contact us</a>
+        </div>
+    </div>
+    </nav>
+
+    <div id="aboutus">
+        
+        <div class="card">
+            <div class="pic">
+                <img src="picture/emilie.png" alt="pic" />
+            </div>
+            <div class="elements">
+                <ul>
+                    <li><span class="title">Name :</span> Emilie</li>
+                    <li><span class="title">First Name :</span> Villiere</li>
+                    <li><span class="title">Age :</span> 25</li>
+                    <li><span class="title">Job :</span>Php specialist</li>
+                    <li><span class="title">Linkedin :</span><a href="https://www.linkedin.com/feed/?trk=hb_signin"></a>ici</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="card">
+                <div class="pic">
+                    <img src="picture/nathan.png" alt="pic" />
+                </div>
+                <div class="elements">
+                    <ul>
+                        <li><span class="title">Name :</span> Nathan</li>
+                        <li><span class="title">First Name :</span> Marcy</li>
+                        <li><span class="title">Age :</span> 25</li>
+                        <li><span class="title">Job :</span>JS specialist</li>
+                        <li><span class="title">Linkedin :</span><a href="https://www.linkedin.com/in/nathan-mrc/">ici</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="card">
+                    <div class="pic">
+                        <img src="picture/dorine.png" alt="pic" />
+                    </div>
+                    <div class="elements">
+                        <ul>
+                            <li><span class="title">Name :</span> Dorine</li>
+                            <li><span class="title">First Name :</span> </li>
+                            <li><span class="title">Age :</span> 25</li>
+                            <li><span class="title">Job :</span>Php specialist</li>
+                            <li><span class="title">Linkedin :</span><a href="https://www.linkedin.com/in/dorine-lombardo-141034123/">ici</a></li>
+                        </ul>
+                    </div>
+                </div>
+    
+                <div class="card">
+                        <div class="pic">
+                            <img src="picture/simon.png" alt="pic" />
+                        </div>
+                        <div class="elements">
+                            <ul>
+                                <li><span class="title">Name :</span> Simon</li>
+                                <li><span class="title">First Name :</span> </li>
+                                <li><span class="title">Age :</span> 25</li>
+                                <li><span class="title">Job :</span>Php specialist</li>
+                                <li><span class="title">Linkedin :</span> http://</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                            <div class="pic">
+                                <img src="picture/moi.png" alt="pic" />
+                            </div>
+                            <div class="elements">
+                                <ul>
+                                    <li><span class="title">Name :</span> Louis</li>
+                                    <li><span class="title">First Name :</span>Allard</li>
+                                    <li><span class="title">Age :</span> 37</li>
+                                    <li><span class="title">Job :</span>JS specialist</li>
+                                    <li><span class="title">Linkedin :</span><a href="https://www.linkedin.com/in/louis-allard-58212b10b/">ici</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+    <div>
+</div>
+
+</body>
